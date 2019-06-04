72829bb1d3a7607da252cf4bf09a8876091a15de
Yves Mauron <ypmauron@gmail.com>
2019-06-04 22:50:17 +0200

    opencv, first steps

--- core/preprocess.py
+++ core/preprocess.py
@@ -2,22 +2,19 @@ import numpy as np
 import cv2
 
 
-def straighten_image(image_path):
+def straighten_image(image):
     """Straighten the image by rotating it
 
     Calculates the angle the image has needs to be rotated in order to be straightened out and then applies the affine
     transformation to actually rotate the image
 
     Args:
-        image_path (string): path to image
+        image (cv.image): image
 
     Returns:
         rotated image
 
     """
-    # read image
-    image = cv2.imread(image_path)
-
     # convert background to grey
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.bitwise_not(gray)
@@ -33,18 +30,19 @@ def straighten_image(image_path):
     # compute a rotated bounding box that contains all
     # coordinates
     coords = np.column_stack(np.where(thresh > 0))
-    angle = cv2.minAreaRect(coords)[-1]
+    rect = cv2.minAreaRect(coords)
 
+    rect_dims = np.array(rect)
     # the `cv2.minAreaRect` function returns values in the
     # range [-90, 0); as the rectangle rotates clockwise the
     # returned angle trends to 0 -- in this special case we
     # need to add 90 degrees to the angle.
     # essentially defines the side the rectangle will rotate to.
-    if angle < -45:
-        angle = -(90 + angle)
+    if rect_dims[1][0] < rect_dims[1][1]:
+        angle = 90 + rect_dims[2]
     # otherwise, just take the inverse of the angle to make it positive
     else:
-        angle = -angle
+        angle = rect_dims[2]
 
     # rotate the image
     (h, w) = image.shape[:2]
@@ -54,29 +52,27 @@ def straighten_image(image_path):
                                    flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 
     # draw the correction angle on the image so we can validate it
-    # meta_info = "Image has been rotated by: {:.2f} degrees".format(angle)
+    meta_info = "Image has been rotated by: {:.2f} degrees".format(angle)
 
     # show the output image
-    return rotated_image
+    return [rotated_image, meta_info]
 
 
-def gaussian_blur(image_path):
+def gaussian_blur(image):
     """Straighten the image by rotating it
 
     Calculates the angle the image has needs to be rotated in order to be straightened out and then applies the affine
     transformation to actually rotate the image
 
     Args:
-        image_path (string): path to image
+        image (cv2.image): image
 
     Returns:
         rotated image
 
     """
-    img = cv2.imread(image_path)
-
     # apply GaussianBlur to smooth image
-    blur = cv2.GaussianBlur(img, (5, 3), 1)
+    blur = cv2.GaussianBlur(image, (5, 3), 1)
 
     # threshold gray region to white (255,255, 255) and sets the rest to black(0,0,0)
     mask = cv2.inRange(blur, (0, 0, 0), (150, 150, 150))
@@ -85,23 +81,21 @@ def gaussian_blur(image_path):
     return 255 - mask
 
 
-def median_blur(image_path):
+def median_blur(image):
     """Straighten the image by rotating it
 
     Calculates the angle the image has needs to be rotated in order to be straightened out and then applies the affine
     transformation to actually rotate the image
 
     Args:
-        image_path (string): path to image
+        image (cv2.image): image
 
     Returns:
         rotated image
 
     """
-    img = cv2.imread(image_path)
-
     # apply MedianBlur to smooth image
-    blur = cv2.medianBlur(img, 3)
+    blur = cv2.medianBlur(image, 3)
 
     # threshold gray region to white (255,255, 255) and sets the rest to black(0,0,0)
     #mask = cv2.inRange(blur, (0, 0, 0), (150, 100, 100))
@@ -109,3 +103,12 @@ def median_blur(image_path):
     # invert the image to have text black-in-white
     return blur
 
+
+def remove_noise_and_smooth(file_name):
+    filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 41)
+    kernel = np.ones((1, 1), np.uint8)
+    opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    img = image_smoothening(img)
+    or_image = cv2.bitwise_or(img, closing)
+    return or_image
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-from core import ocr
-from core import utils
+from core import ocr,utils, preprocess, table
+from itertools import chain
+import cv2
+import os
+import numpy as np
+import json
 # -------------
 # testing
 # -------------
+
+if __name__ == '__main__':
+    with open('input.json') as json_file:
+        template = json.load(json_file)
+
+    document_path = template['document_path']
+    classification_bbox = template['classification_bbox']
+
+    names = []
+    bounding_boxes = []
+    for box in template['bounding_boxes']:
+        names.append(box['name'])
+        bounding_boxes.append(tuple(box['bbox']))
+
+    # draw boxes in images for illustration purposes
+    utils.draw_boxes(document_path, bounding_boxes, './misc/extraction_zones.jpg')
+    img = cv2.GaussianBlur(cv2.imread(document_path), (5, 5), 0)
+    # if boxes are correct, then perform ocr indexing on them
+    print(ocr.perform_ocr(input_path=document_path, coordinates=bounding_boxes))
+
 # get the pdf
-pdf_path = './docs/scans.pdf'
+#pdf_path = './docs/scans.pdf'
 # and extract each page and convert it into a jpeg
-utils.extract_pages(pdf_path, 'img')
-
-# define image path (here first image)
-image_path = 'img/page_0.jpg'
-# coordinates from top left
-# these can easily be defined using gimp
-zones = [
-    (240, 900, 1400, 975),
-    (237, 2220, 456, 2253)
-]
-
-# draw boxes in images for illustration purposes
-utils.draw_boxes(image_path, zones, './misc/extraction_zones.jpg')
-
-# if boxes are correct, then perform ocr indexing on them
-print(ocr.perform_ocr(input_path=image_path, coordinates=zones))
-
-
-
-
+#utils.extract_pages(pdf_path, 'img')
